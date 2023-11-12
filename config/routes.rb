dec82d48bcc6fb03f44661447df0045245041bb9
Matthew Bernhardt <mjbernha@mit.edu>
2023-11-12 16:55:32 -0500

    Static infrastructure
    This adds a static controller, with an index route to act as the
    application's homepage. There are some rudimentary styles for the
    rendering of the menu bar as well, and a simple navigation menu.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :characters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'static#index'
 end
